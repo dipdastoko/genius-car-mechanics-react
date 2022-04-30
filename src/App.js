3afbf5a91adb60810b5ae9e877949609558187d9
dipdastoko <dipdastoko@gmail.com>
2022-04-30 14:08:15 +0600

    notfound pic added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home></Home>}>
+          </Route>
+          <Route path='/home' element={<Home></Home>}>
+          </Route>
+          <Route path='*' element={<NotFound></NotFound>}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
