b580a392142fd457bb9d96cd960f88a1610ca409
dipdastoko <dipdastoko@gmail.com>
2022-06-16 09:34:24 +0600

    axios post method added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import './AddService.css';
 
 const AddService = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        axios.post('http://localhost:5000/services', data)
+            .then(res => {
+                console.log(res);
+            })
+    };
     return (
         <div className='add-service'>
             <h2>Add a service</h2>
