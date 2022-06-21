74e9ef5e3a7ab893084e1b56984036e440042821
dipdastoko <dipdastoko@gmail.com>
2022-06-21 21:49:03 +0600

    lsd

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Booking = () => {
     const { serviceId } = useParams();
+    const [service, setService] = useState({});
+
+    useEffect(() => {
+        fetch(`https://immense-sierra-19949.herokuapp.com/${serviceId}`)
+            .then(res => res.json())
+            .then(data => setService(data));
+    }, [])
     return (
         <div>
-            <h2>This is booking {serviceId}</h2>
+            <h2>Details of {service.Name}</h2>
+            <h3>Price: ${service.Price}</h3>
         </div>
     );
 };
